ea5dc47179bb98bb22a3798906521a4caff6a114
leucanthos <leucanthos@outlook.com>
2025-12-15 09:27:30 +0800

    Transfer learning demo project: Data transformation pipeline for cross-platform customer flow prediction

--- src/data_transformation/load_data.py
+++ src/data_transformation/load_data.py
@@ -2,13 +2,13 @@ import polars as pl
 
 def load_inputs(input_dir)->dict[str,pl.LazyFrame]:
     datasets_pth = {
-        "air_reserve": input_dir / "air_reserve.csv",
-        "air_store_info": input_dir / "air_store_info.csv",
-        "air_visit_data": input_dir / "air_visit_data.csv",
-        "hpg_reserve": input_dir / "hpg_reserve.csv",
-        "hpg_store_info": input_dir / "hpg_store_info.csv",
-        "store_id_relation": input_dir / "store_id_relation.csv",
-        "date_info": input_dir / "date_info.csv",
+        "air_reserve": input_dir / "air_reserve.parquet",
+        "air_store_info": input_dir / "air_store_info.parquet",
+        "air_visit_data": input_dir / "air_visit_data.parquet",
+        "hpg_reserve": input_dir / "hpg_reserve.parquet",
+        "hpg_store_info": input_dir / "hpg_store_info.parquet",
+        "store_id_relation": input_dir / "store_id_relation.parquet",
+        "date_info": input_dir / "date_info.parquet",
     }
-    datasets = {name: pl.scan_csv(pth) for name, pth in datasets_pth.items()}
+    datasets = {name: pl.scan_parquet(pth) for name, pth in datasets_pth.items()}
     return datasets
